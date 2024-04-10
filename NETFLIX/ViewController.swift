b4ae510c65f01001112725e2ba432c3d752a6894
akash <akashs@gmail.com>
2024-04-10 10:57:38 +0530

    Initial Commit

---
+++ NETFLIX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NETFLIX
+//
+//  Created by Shruti Makwana on 10/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
